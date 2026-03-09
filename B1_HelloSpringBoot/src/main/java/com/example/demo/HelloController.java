fc831bc7caccd1488c6607114f907c1ce9b1c58f
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-03-09 17:05:18 +0700

    cấu hình lại cổng máy chủ do cổng 8080 không chạy được.

--- B1_HelloSpringBoot/src/main/java/com/example/demo/HelloController.java
+++ B1_HelloSpringBoot/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World! Chào mừng bạn đến với Spring Boot!";
+	}
 }
