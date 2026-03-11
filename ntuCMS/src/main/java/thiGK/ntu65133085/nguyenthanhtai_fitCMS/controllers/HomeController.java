9d6d6ce5cd4ef994f4c943561177aead135d9e9d
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-03-11 15:50:44 +0700

    .

--- ntuCMS/src/main/java/thiGK/ntu65133085/nguyenthanhtai_fitCMS/controllers/HomeController.java
+++ ntuCMS/src/main/java/thiGK/ntu65133085/nguyenthanhtai_fitCMS/controllers/HomeController.java
@@ -1,15 +1,34 @@
 package thiGK.ntu65133085.nguyenthanhtai_fitCMS.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import thiGK.ntu65133085.nguyenthanhtai_fitCMS.models.Page;
+import thiGK.ntu65133085.nguyenthanhtai_fitCMS.models.Post;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/dash-board")
+	ArrayList<Page> dsTrang = new ArrayList<Page>();
+	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
+
+	public HomeController() {
+		dsTrang.add(new Page(1, "Trang chủ", "home", "Nội dung trang chủ", 0));
+		dsTrang.add(new Page(2, "Giới thiệu", "about", "Giới thiệu về chúng tôi", 0));
+		dsTrang.add(new Page(3, "Sản phẩm", "product", "Danh sách sản phẩm", 0));
+	}
+
+	@GetMapping({ "/", "/dashboard", "/dasdboard" })
 	public String home() {
 		return "index";
 	}
-//	ArrayList<Page> dsTrang = new ArrayList<Page>();
-//	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
+
+	@GetMapping("/page/all")
+	public String getAllPages(ModelMap m) {
+		m.addAttribute("listPages", dsTrang);
+		return "allPage";
+	}
 }
