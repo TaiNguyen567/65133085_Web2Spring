e9961b16e4ef485805a2b75de87554bcfa3550e9
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-03-11 14:37:48 +0700

    tạo dự án thi thử giứa kì môn lập trình web 2 Spring Boot

---
+++ ntuCMS/src/main/java/thiGK/ntu65133085/nguyenthanhtai_fitCMS/NtuCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133085.nguyenthanhtai_fitCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtuCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtuCmsApplication.class, args);
+	}
+
+}
---
+++ ntuCMS/src/test/java/thiGK/ntu65133085/nguyenthanhtai_fitCMS/NtuCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133085.nguyenthanhtai_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtuCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
