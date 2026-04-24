3825a0ff6940fc03df094d50124918bb1fce8952
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-04-24 13:42:13 +0700

    Lần thứ 1

---
+++ ThiGK2_NguyenThanhTai/src/main/java/thigk2/nguyenthanhtai/ThiGk2NguyenThanhTaiApplication.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenthanhtai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGk2NguyenThanhTaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGk2NguyenThanhTaiApplication.class, args);
+	}
+
+}
---
+++ ThiGK2_NguyenThanhTai/src/test/java/thigk2/nguyenthanhtai/ThiGk2NguyenThanhTaiApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenthanhtai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGk2NguyenThanhTaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
